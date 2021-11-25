42b3a9b73dfb0a9240ade2a997d867993bedfa49
joze08 <riosjunior270@gmail.com>
2021-11-25 10:31:40 -0300

    inicio

---
+++ app/Controllers/Http/MessagesController.ts
@@ -0,0 +1,46 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Message from 'App/Models/Message'
+
+export default class MessagesController {
+  public async index({ }: HttpContextContract) {
+    const messages = await Message.query().preload('user');
+    return messages;
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    try {
+      const data = request.only(["texto", "user_id"]);
+      const message = await Message.create(data);
+      return message;
+    } catch (error) {
+      response.status(500).send("Erro ao enviar a mensagem");
+    }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const message = await Message.findOrFail(params.id);
+    return message;
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const message = await Message.findOrFail(params.id);
+      const { texto } = request.only(["texto"]);
+      message.texto = texto;
+      await message.save();
+      return message;
+    } catch (error) {
+      response.status(500).send("Erro ao atualizar a mensagem");
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      const message = await Message.findOrFail(params.id);
+      await message.delete()
+      return message;
+    } catch (error) {
+      response.status(500).send("Erro ao apagar a mensagem!")
+    }
+  }
+}
