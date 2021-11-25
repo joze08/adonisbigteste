f8c8d3ab19f08bffe6889829c66263e87bb7d6e2
joze08 <riosjunior270@gmail.com>
2021-11-25 13:01:40 -0300

    id correcao

--- database/migrations/1633520880726_users.ts
+++ database/migrations/1633520880726_users.ts
@@ -5,7 +5,7 @@ export default class UsersSchema extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.increments('id')
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
